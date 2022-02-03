f42952e495574d786168fad39af70318cc49324a
DamonHayes <92762805+DamonHayes@users.noreply.github.com>
2022-02-02 19:49:47 -0600

    Moving the player
    I added keyEventListeners to allow the player to move left and right.

--- js/index.js
+++ js/index.js
@@ -4,6 +4,12 @@ var context;
 let canvasH = 80 * window.innerHeight / 100;
 let canvasW = 80 * window.innerWidth / 100;
 
+//input vars
+var upKey;
+var rightKey;
+var leftKey;
+var downKey;
+
 //create game vars
 var gameLoop;
 var player;
@@ -12,17 +18,15 @@ window.onload = function() {
 	//assin canvas and context vars
 	canvas = document.getElementById('gameCanvas');
 	context = canvas.getContext('2d');
-	
-	console.log(canvasH)
-	console.log(canvasW)
-	
+
 	canvas.height = canvasH;
 	canvas.width = canvasW;
 	
 	//set up key listeners
+	setupInputs()
 	
 	//create player
-	player = new Player(100, 400);
+	player = new Player(50, canvasH - 100);
 	
 	//start game loop
 	gameLoop = setInterval(step, 1000/30);
@@ -47,4 +51,29 @@ function draw() {
 
 	//draw the player
 	player.draw();
+}
+
+function setupInputs(){
+	document.addEventListener('keydown', function(event){
+		if (event.key === 'w' || event.key === 'ArrowUp'){
+			upKey = true;
+		}else if (event.key === 'a' || event.key === 'ArrowLeft'){
+			leftKey = true;
+		}else if (event.key === 'd' || event.key === 'ArrowRight'){
+			rightKey = true;
+		}else if (event.key === 's' || event.key === 'ArrowDown'){
+			downKey = true;
+		}
+	});
+	document.addEventListener('keyup', function(event){
+		if (event.key === 'w' || event.key === 'ArrowUp'){
+			upKey = false;
+		}else if (event.key === 'a' || event.key === 'ArrowLeft'){
+			leftKey = false;
+		}else if (event.key === 'd' || event.key === 'ArrowRight'){
+			rightKey = false;
+		}else if (event.key === 's' || event.key === 'ArrowDown'){
+			downKey = false;
+		}
+	});
 }
--- js/player.js
+++ js/player.js
@@ -1,11 +1,31 @@
 function Player(x,y){
 	this.x = x;
 	this.y = y;
+	this.xSpeed = 0;
+	this.ySpeed = 0;
+	this.friction = 0.6;
+	this.maxSpeed = 10;
 	this.width = 50;
 	this.height = 100;
+	this.active = true;
 	
 	this.step = function() {
-		
+		if (this.active) {
+			//horizontal movement
+			if (!leftKey && !rightKey || leftKey && rightKey){
+				//slow down
+				this.xSpeed *= this.friction;
+			}else if (rightKey) {
+				this.xSpeed ++;
+			}else if (leftKey){
+				this.xSpeed --;
+			}
+			//vertical movement
+			
+			//correct speed
+			this.x += this.xSpeed;
+			this.y += this.ySpeed;
+		}
 	}
 	
 	this.draw = function() {
